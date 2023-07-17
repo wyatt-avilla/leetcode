e3a0d3652cb2f95f83a1f0bd6964887e08c8f691
wyatt-avilla <wyattmurphy1@gmail.com>
2023-07-16 21:23:47 -0700

    btreedepth and matrix reshape

---
+++ python/matrixReshape.py
@@ -0,0 +1,24 @@
+from typing import List
+from collections import deque
+import itertools
+
+
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        flattened = deque(list(itertools.chain(*mat)))
+        reshaped = []
+
+        if r * c != len(flattened):
+            return mat
+
+        for row in range(r):
+            reshaped.append([])
+            for col in range(c):
+                reshaped[row].append(flattened.popleft())
+
+        return reshaped
+
+
+nlist = [[1, 2], [3, 4]]
+
+print(Solution().matrixReshape(nlist, 2, 4))
