5c129e60472fe6b3aa13d239a85bf00d59ba551c
wyatt-avilla <wyattmurphy1@gmail.com>
2024-03-30 14:53:55 -0700

    nth tribonacci number

---
+++ python/n-th-tribonacci-number.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/n-th-tribonacci-number/
+
+from functools import cache
+
+
+class Solution:
+    @cache
+    def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n in (1, 2):
+            return 1
+
+        return sum(self.tribonacci(x) for x in range(n - 3, n))
+
+
+assert Solution().tribonacci(4) == 4
+assert Solution().tribonacci(25) == 1389537
