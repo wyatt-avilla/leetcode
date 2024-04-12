536b8192b4d7ecd35b1958bf87389e428b30e760
wyatt-avilla <wyattmurphy1@gmail.com>
2024-04-12 12:11:27 -0700

    initializer list functionality in `ListNode`

--- cpp/data_structures.h
+++ cpp/data_structures.h
@@ -5,6 +5,16 @@ struct ListNode {
     ListNode* next;
     ListNode() : val(0), next(nullptr) {}
     ListNode(int x, ListNode* next) : val(x), next(next) {}
+    ListNode(std::initializer_list<int> values) : val(0), next(nullptr) {
+        ListNode* current = this;
+        for (int value : values) {
+            current->val = value;
+            if (value != *(values.end() - 1)) {
+                current->next = new ListNode();
+                current = current->next;
+            }
+        }
+    }
 
     friend std::ostream& operator<<(std::ostream& os, const ListNode* node) {
         while (node != nullptr) {
