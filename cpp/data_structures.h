9a3d0f32466756c8c6143094a41a3ffd400807be
wyatt-avilla <wyattmurphy1@gmail.com>
2024-05-25 10:44:32 -0700

    `TreeNode` in cpp data structure header

--- cpp/data_structures.h
+++ cpp/data_structures.h
@@ -46,3 +46,13 @@ struct ListNode {
         return os;
     }
 };
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right)
+        : val(x), left(left), right(right) {}
+};
