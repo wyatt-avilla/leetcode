95b11a27e6604232195b9bc3a410906ff9cabf9d
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-04-06 22:14:04 -0700

    rename dir

--- rust/valid-parenthesis/target/debug/deps/valid_parenthesis-80cac242fed12b2d.d
+++
@@ -1,5 +0,0 @@
-/home/wyatt/downloads/leetcode/rust/valid-parenthesis/target/debug/deps/valid_parenthesis-80cac242fed12b2d.rmeta: src/main.rs
-
-/home/wyatt/downloads/leetcode/rust/valid-parenthesis/target/debug/deps/valid_parenthesis-80cac242fed12b2d.d: src/main.rs
-
-src/main.rs:
