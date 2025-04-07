6e74b46c5203a6f5904614c3bd90f721cf31d79d
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-04-06 22:33:26 -0700

    check if a word occurs as a prefix of any word in a sentence

--- rust/src/problems/check_if_a_word_occurs_as_a_prefix_of_any_word_in_a_sentence.rs
+++ rust/src/problems/check_if_a_word_occurs_as_a_prefix_of_any_word_in_a_sentence.rs
@@ -14,37 +14,28 @@ impl Solution {
     }
 }
 
-fn main() {
-    println!("main executed")
-}
-
-// Import the necessary modules
 #[cfg(test)]
 mod tests {
-    // Import the Solution struct (assuming it's in the same module or crate)
     use super::Solution;
 
-    // Test case 1
     #[test]
-    fn case1() {
+    fn case_1() {
         let sentence = String::from("i love eating burger");
         let search_word = String::from("burg");
         let result = Solution::is_prefix_of_word(sentence, search_word);
         assert_eq!(result, 4);
     }
 
-    // Test case 2
     #[test]
-    fn case2() {
+    fn case_2() {
         let sentence = String::from("this problem is an easy problem");
         let search_word = String::from("pro");
         let result = Solution::is_prefix_of_word(sentence, search_word);
         assert_eq!(result, 2);
     }
 
-    // Test case 3
     #[test]
-    fn case3() {
+    fn case_3() {
         let sentence = String::from("i am tired");
         let search_word = String::from("you");
         let result = Solution::is_prefix_of_word(sentence, search_word);
--- rust/src/problems/mod.rs
+++ rust/src/problems/mod.rs
@@ -2,3 +2,4 @@
 
 pub mod apply_discount_every_n_orders;
 pub mod camelcase_matching;
+pub mod check_if_a_word_occurs_as_a_prefix_of_any_word_in_a_sentence;
