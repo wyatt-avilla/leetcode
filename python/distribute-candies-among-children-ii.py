c8de22e6f98869ace19c7d91ecca5c6a468ccfc2
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-05-31 22:36:49 -0700

    candies among children fast

--- python/distribute-candies-among-children-ii.py
+++ python/distribute-candies-among-children-ii.py
@@ -1,12 +1,19 @@
 # https://leetcode.com/problems/distribute-candies-among-children-ii/
 
+from math import comb
+
 
 class Solution:
     def distributeCandies(self, n: int, limit: int) -> int:
-        return sum(
-            len([None for j in range(min(limit, n - i) + 1) if n - i - j <= limit])
-            for i in range(min(limit, n) + 1)
-        )
+        def fn(x: int) -> int:
+            return comb(max(0, n - x * (limit + 1) + 2), 2)
+
+        total_dists = comb(n + 2, 2)
+        geq_one_over = 3 * fn(1)
+        geq_two_over = 3 * fn(2)
+        all_over = fn(3)
+
+        return total_dists - geq_one_over + geq_two_over - all_over
 
 
 if __name__ == "__main__":
