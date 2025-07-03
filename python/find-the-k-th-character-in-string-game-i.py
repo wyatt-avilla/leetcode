17c999b7dd5f394fac1acedd99d78be4769e980f
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-07-02 17:14:00 -0700

    find the k th character in string game i

---
+++ python/find-the-k-th-character-in-string-game-i.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/find-the-k-th-character-in-string-game-i/
+
+
+class Solution:
+    def kthCharacter(self, k: int) -> str:
+        precomputed = "abbcbccdbccdcddebccdcddecddedeefbccdcddecddedeefcddedeefdeefeffgbccdcddecddedeefcddedeefdeefeffgcddedeefdeefeffgdeefeffgeffgfgghbccdcddecddedeefcddedeefdeefeffgcddedeefdeefeffgdeefeffgeffgfgghcddedeefdeefeffgdeefeffgeffgfgghdeefeffgeffgfggheffgfgghfgghghhibccdcddecddedeefcddedeefdeefeffgcddedeefdeefeffgdeefeffgeffgfgghcddedeefdeefeffgdeefeffgeffgfgghdeefeffgeffgfggheffgfgghfgghghhicddedeefdeefeffgdeefeffgeffgfgghdeefeffgeffgfggheffgfgghfgghghhideefeffgeffgfggheffgfgghfgghghhieffgfgghfgghghhifgghghhighhihiij"
+
+        return precomputed[k - 1]
+
+
+if __name__ == "__main__":
+    assert Solution().kthCharacter(5) == "b"
+    assert Solution().kthCharacter(10) == "c"
