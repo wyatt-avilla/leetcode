3ebfd47716987866b9a67fc1f08d9b918e036ff2
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-08-08 17:03:43 -0700

    power of two

---
+++ python/power-of-two.py
@@ -0,0 +1,47 @@
+# https://leetcode.com/problems/power-of-two/
+
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        powers_in_range = {
+            1,
+            2,
+            4,
+            8,
+            16,
+            32,
+            64,
+            128,
+            256,
+            512,
+            1024,
+            2048,
+            4096,
+            8192,
+            16384,
+            32768,
+            65536,
+            131072,
+            262144,
+            524288,
+            1048576,
+            2097152,
+            4194304,
+            8388608,
+            16777216,
+            33554432,
+            67108864,
+            134217728,
+            268435456,
+            536870912,
+            1073741824,
+            2147483648,
+        }
+
+        return n in powers_in_range
+
+
+if __name__ == "__main__":
+    assert Solution().isPowerOfTwo(1)
+    assert Solution().isPowerOfTwo(16)
+    assert not Solution().isPowerOfTwo(3)
