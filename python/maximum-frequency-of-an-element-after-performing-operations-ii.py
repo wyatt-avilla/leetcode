82525d0e2122e1455706649406d2f8f23d44ebbb
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-10-20 22:20:53 -0700

    maximum frequency of an element after performing operations ii

---
+++ python/maximum-frequency-of-an-element-after-performing-operations-ii.py
@@ -0,0 +1,40 @@
+# https://leetcode.com/problems/maximum-frequency-of-an-element-after-performing-operations-ii/
+
+from collections import Counter, defaultdict
+
+
+class Solution:
+    def maxFrequency(self, nums: list[int], k: int, num_operations: int) -> int:
+        counts = Counter(nums)
+        sweep: dict[int, int] = defaultdict(int)
+        n = 10**9 + 2
+        left = n
+        right = 0
+
+        for num in nums:
+            x_0 = max(1, num - k)
+            x_n = min(n - 1, num + k + 1)
+
+            sweep[x_0] += 1
+            sweep[x_n] -= 1
+
+            left = min(left, x_0)
+            right = max(right, x_n)
+
+        coords = sorted(
+            {*(key for key in sweep if key >= left and key <= right), *nums},
+        )
+
+        count = ans = 0
+        for i in coords:
+            count += sweep[i]
+            ans = max(ans, counts[i] + min(count - counts[i], num_operations))
+
+        return ans
+
+
+if __name__ == "__main__":
+    assert Solution().maxFrequency([88, 53], 27, 2) == 2
+    assert Solution().maxFrequency([1, 4, 5], 1, 2) == 2
+    assert Solution().maxFrequency([5, 11, 20, 20], 5, 1) == 2
+    assert Solution().maxFrequency([999999997, 999999999, 999999999], 999999999, 2) == 3
